08d67b36d775190f4fe29eaf69f9442a0a66e492
Marcelo Zepedeo <marcelo.zepedeo@globalpay.com>
2023-10-06 19:38:45 -0300

    Prueba 2

--- NpgsqlPoc/NpgsqlPoc/Controllers/ValuesController1.cs
+++ NpgsqlPoc/NpgsqlPoc/Controllers/ValuesController1.cs
@@ -5,35 +5,35 @@
 namespace NpgsqlPoc.Controllers;
 [Route("api/[controller]")]
 [ApiController]
-public class ValuesController : ControllerBase
+public class ValuesController1 : ControllerBase
 {
-    // GET: api/<ValuesController>
+    // GET: api/<ValuesController1>
     [HttpGet]
     public IEnumerable<string> Get()
     {
-        return new string[] { "value1", "value2", "value3" };
+        return new string[] { "value1", "value2" };
     }
 
-    // GET api/<ValuesController>/5
+    // GET api/<ValuesController1>/5
     [HttpGet("{id}")]
     public string Get(int id)
     {
         return "value";
     }
 
-    // POST api/<ValuesController>
+    // POST api/<ValuesController1>
     [HttpPost]
     public void Post([FromBody] string value)
     {
     }
 
-    // PUT api/<ValuesController>/5
+    // PUT api/<ValuesController1>/5
     [HttpPut("{id}")]
     public void Put(int id, [FromBody] string value)
     {
     }
 
-    // DELETE api/<ValuesController>/5
+    // DELETE api/<ValuesController1>/5
     [HttpDelete("{id}")]
     public void Delete(int id)
     {
